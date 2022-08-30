75f5991b3b30cd02492d298b60774434903e5974
orangecheetopuff <rochedjohnson@gmail.com>
2022-08-30 10:41:27 +0200

    type your message here

--- script.js
+++ script.js
@@ -23,5 +23,26 @@ let weather = {
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + name + "')"
     },
-}
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    }
+};
+
+document
+    .querySelector(".search button")
+    .addEventListener("click", function () {
+        weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    })
+
+weather.fetchWeather("Cape Town");
